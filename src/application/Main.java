f0e8d94c8608b2565988259945ea680ff188dc54
Ahmed Muhammed AbdelHakim <70027345+Kaiimoo@users.noreply.github.com>
2022-05-20 07:49:45 +0200

    Initial Commit
    - Setup The Project Initial Libraries
    - Simple Initial GUI

---
+++ src/application/Main.java
@@ -0,0 +1,34 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setTitle("KENKEN Puzzle Solver");
+			//Image icon = new Image("icon.png");
+			//primaryStage.getIcons().add(icon);
+			primaryStage.setResizable(false);
+			primaryStage.setMinWidth(920);
+			primaryStage.setMinHeight(640);	
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module KenKen {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
